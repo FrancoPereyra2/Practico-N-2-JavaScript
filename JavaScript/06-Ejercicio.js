5a182bad407f0caf0535b4382462b202d9cd3009
Franco Pereyra <francopereyra325@gmail.com>
2024-07-17 14:07:18 -0300

    ejercicio 6 terminado

---
+++ JavaScript/06-Ejercicio.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt('Ingrese un numero: '));
+for(let i = 1; i <= 30; i++){
+    for(let j = 0; j < i; j++){
+        document.write(`${i}`);
+    }
+    document.write(`<br>`);
+}
